d0c1db704849989ae1d112abe3f20489ac3f18a7
killbit <74510491+xkillbit@users.noreply.github.com>
2024-05-10 16:18:20 -0400

    Update domain_password_analysis.py

--- domain_password_analysis.py
+++ domain_password_analysis.py
@@ -1,29 +1,30 @@
-#!/usr/bin/env python
-# USAGE
-# MAKE SURE TO ADD HEADERS to the begining of your domain hash csv dump file: Domain,UserName,LMHash,NTHash
-
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\xkill\Documents\my_scripts\password_analysis\domain_hash_dump.csv')
+df = pd.read_csv(r'C:\Users\xkill\Documents\my_scripts\password_analysis\passwordhashes.csv')
 
 
 # Filter out rows where Domain contains '$'
+#filtered_df = df[(~df['Domain'].str.contains('\$'))or(~df['UserName'].str.contains('$'))]
 filtered_df = df[~(df['Domain'].str.contains('\$', na=False) | df['UserName'].str.contains('\$', na=False))]
 
 # Find duplicates based on NTHash
 duplicate_hashes = filtered_df[filtered_df.duplicated('NTHash', keep=False)]
-
 # Sort by UserName
 sorted_duplicates = duplicate_hashes.sort_values(by='NTHash')
-
-lmhash_counts = df.groupby('LMHash').size().reset_index(name='Count')
-lmhash_counts = lmhash_counts.sort_values('Count', ascending=False)
-
+print(sorted_duplicates)
 
 # Write sorted DataFrame to CSV
 sorted_duplicates.to_csv('sorted_duplicates.csv', index=False)
 print("DataFrame is written to sorted_duplicates CSV file successfully.")
 
+
+# Group by 'LMHash' and aggregate
+result = df.groupby('LMHash').agg({
+    'NTHash': 'size',  # Count the occurrences of each hash
+    'UserName': lambda x: ', '.join(x.dropna().astype(str))  # Concatenate the user names, handling NaN values
+}).rename(columns={'NTHash': 'Count', 'UserName': 'AccountsWithPassword'})
+sort_results=result.sort_values('Count', ascending=False)
+
 # Write LMHash Count ANalysis DataFrame to CSV
-lmhash_counts.to_csv('lmhash_counts.csv', index=False)
+sort_results.to_csv('lmhash_counts.csv', index=True)
 print("lmhash_counts is written to lmhash_counts CSV file successfully.")
