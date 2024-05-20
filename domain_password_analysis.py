ca9b505073f80eff07032359b8f8a0d9ffb5ae07
killbit <74510491+xkillbit@users.noreply.github.com>
2024-05-20 12:39:46 -0400

    Update domain_password_analysis.py

--- domain_password_analysis.py
+++ domain_password_analysis.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\xkill\Documents\my_scripts\password_analysis\passwordhashes.csv')
 
+df = pd.read_csv(r'C:\Users\xkill\path\to\password_dump.csv')
+# password_dump must have metadata of Domain, UserName, LMHash, NTHash
 
 # Filter out rows where Domain contains '$'
 #filtered_df = df[(~df['Domain'].str.contains('\$'))or(~df['UserName'].str.contains('$'))]
@@ -18,13 +19,13 @@ sorted_duplicates.to_csv('sorted_duplicates.csv', index=False)
 print("DataFrame is written to sorted_duplicates CSV file successfully.")
 
 
-# Group by 'LMHash' and aggregate
-result = df.groupby('LMHash').agg({
+# Group by 'NTHash' and aggregate
+result = df.groupby('NTHash').agg({
     'NTHash': 'size',  # Count the occurrences of each hash
     'UserName': lambda x: ', '.join(x.dropna().astype(str))  # Concatenate the user names, handling NaN values
 }).rename(columns={'NTHash': 'Count', 'UserName': 'AccountsWithPassword'})
 sort_results=result.sort_values('Count', ascending=False)
 
-# Write LMHash Count ANalysis DataFrame to CSV
-sort_results.to_csv('lmhash_counts.csv', index=True)
+# Write NTHash Count ANalysis DataFrame to CSV
+sort_results.to_csv('NTHash_counts.csv', index=True)
 print("lmhash_counts is written to lmhash_counts CSV file successfully.")
